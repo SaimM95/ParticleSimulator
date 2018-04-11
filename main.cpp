5795804db5c3f124f8ee8e1368c6ba54dd7804a3
Saim Malik <saim95@gmail.com>
2018-04-11 18:39:15 -0400

    added generate random array

--- main.cpp
+++ main.cpp
@@ -5,12 +5,39 @@
 
 #include <mpi.h>
 #include <math.h>
+#include <time.h>
 
 #include "vector3d.h"
 #include "savebmp.h"
 #include "properties.h"
 
 #define epsilon 0.000000000000000222
+
+double* genTestArr(int size, int start) {
+  double* positions = (double*) malloc(sizeof(double) * size);
+  int val = start;
+  for (int i = 0; i < size; ++i) {
+    if (i > 0 && i%2 == 0) {
+      val++;
+    }
+    positions[i] = (double) val;
+  }
+  return positions;
+}
+
+double* genRandomArr(int size, double min, double max) {
+  double* positions = (double*) malloc(sizeof(double) * size);
+  for (int i = 0; i < size; ++i) {
+    positions[i] = (drand48() * (max - min)) + min;
+  }
+  return positions;
+}
+
+void printVecArr(double *arr, int size) {
+  for (int i = 0; i < size; i += 2) {
+    printf("(%.4f,%.4f)\n", arr[i], arr[i+1]);
+  }
+}
 
 int main(int argc, char* argv[]){
 
@@ -25,9 +52,9 @@ int main(int argc, char* argv[]){
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &p);
 
-  int numParticlesLight = atoi(argv[1]);
-  // int numParticleMedium = argv[2];
-  // int numParticleHeavy = argv[3];
+  int numParticlesLight = 2;//atoi(argv[1]);
+  int numParticleMedium = 2;//atoi(argv[1]);
+  int numParticleHeavy = 2;//atoi(argv[1]);
   // int numSteps = argv[4];
   // int subSteps = argv[5];
   // double timeSubStep = argv[6];
@@ -41,18 +68,29 @@ int main(int argc, char* argv[]){
 
   //root node stuff goes here
   if(my_rank == 0){
+    // set seed for random number generation
+    srand48(time(NULL));
 
+    int numParticles = 4;//numParticlesLight + numParticleMedium + numParticleHeavy;
 
+    printf("Positions:\n");
+    int size = numParticles * 2;
+    double *positions = genTestArr(size, 0);// genRandomArr(size, 0, 10);
+    printVecArr(positions, size);    
+
+    printf("Velocities:\n");
+    double *velocities = genTestArr(size, 1);// genRandomArr(size, 0, 10);
+    printVecArr(velocities, size);
 
     //almost done, just save the image
-    saveBMP(argv[9], image, width, height);
+    // saveBMP(argv[9], image, width, height);
   }
   //all other nodes do this
-  else{
+  else {
 
   }
 
-  free(image);
+  // free(image);
 
   MPI_Finalize();
   return 0;
