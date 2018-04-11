36fa7966465b4bb9748e3491a61ffa08c82a61f2
Khan Ahmed <khana97@mcmaster.ca>
2018-04-11 19:13:38 -0400

    generates an image now

--- main.cpp
+++ main.cpp
@@ -12,6 +12,44 @@
 
 #define epsilon 0.000000000000000222
 
+
+void drawParticle(unsigned char* img, int w,int h,int x, int y, int r, int cr,int cg, int cb){
+  int minX = x - r;
+  int maxX = x + r;
+  printf("going to draw particle\n");
+  for(int x = minX; x < maxX; x++){
+    if(x < 0) continue;
+    if(x >= w) break;
+    //int minY = y - sqrt(r*r - x*x);
+    //int maxY = y + sqrt(z);
+    int minY = y - r;
+    int maxY = y + r;
+    for(int y = minY; y < maxY; y++){
+      if(y < 0) continue;
+      if(y >= h) break;
+      img[(x + y*w)*3] = cr;
+      img[(x + y*w)*3+1] = cg;
+      img[(x + y*w)*3+2] = cb;
+    }
+  }
+}
+unsigned char* createImage(double* pos, int w,int h, int nl, int nm, int nh){
+  int size = w*h*3;
+  unsigned char* img = (unsigned char *)malloc(sizeof(char)*size);
+  for(int i =0; i < size; i++){
+    img[i] = 0;
+  }
+
+  int radius = 2;
+  for(int i =0; i < nl; i++){
+    int x = (int)pos[i*2];
+    int y = (int)pos[i*2+1];
+    drawParticle(img,w,h,x,y,radius,255,255,255);
+    printf("setting value at (%i,%i) to 255\n", x,y);
+  }
+
+  return img;
+}
 int main(int argc, char* argv[]){
 
   if( argc != 10){
@@ -25,34 +63,38 @@ int main(int argc, char* argv[]){
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &p);
 
-  int numParticlesLight = atoi(argv[1]);
-  // int numParticleMedium = argv[2];
-  // int numParticleHeavy = argv[3];
-  // int numSteps = argv[4];
-  // int subSteps = argv[5];
-  // double timeSubStep = argv[6];
-  // int imageWidth = argv[7];
-  // int imageHeight = argv[8];
+  int nl = atoi(argv[1]);
+  int nm = atoi(argv[2]);
+  int nh = atoi(argv[3]);
+  int numSteps = atoi(argv[4]);
+  int subSteps = atoi(argv[5]);
+  double timeSubStep = atof(argv[6]);
+  int width = atoi(argv[7]);
+  int height = atoi(argv[8]);
   // int imageFilenamePrefix = argv[9];
 
-  int width, height;
-
-  unsigned char* image;
+  //int width, height;
+  double * pos = (double*)malloc(sizeof(double) * 2 * 10);
+  for(int i =0; i < 10; i++){
+    pos[i*2] = 5;
+    pos[i*2+1] = 5;
+  }
+  unsigned char* img = createImage(pos, width, height, nl,nm,nh);
 
   //root node stuff goes here
   if(my_rank == 0){
 
 
-
     //almost done, just save the image
-    saveBMP(argv[9], image, width, height);
+    printf("width: %i, height:%i\n", width, height);
+    saveBMP(argv[9], img, width, height);
   }
   //all other nodes do this
   else{
 
   }
 
-  free(image);
+  free(img);
 
   MPI_Finalize();
   return 0;
