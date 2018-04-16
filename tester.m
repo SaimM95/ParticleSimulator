0cdc50a202ad285e723881cebfdb99ee40eb4ff8
Saim Malik <saim95@gmail.com>
2018-04-16 17:28:15 -0400

    updated tester

--- tester.m
+++ tester.m
@@ -1,11 +1,15 @@
 function tester(iterations)
-	P = [500,300;500,500];
-	V = [0,0;0,0];
-	M = [1;15000];
+	% P = [500,300;500,500];
+	% V = [0,0;0,0];
+	% M = [1;10^17];
 	H = 1;
 
+	P = [712.4900,548.8440;1453.8590,506.4524];
+	V = [4.4121,-4.1096;4.4690,-4.8080];
+	M = [11.7696;11.8736];
+
 	for t=1:iterations
-		fprintf('Time Step = %d\n', t);
+		% fprintf('Time Step = %d\n', t);
 
 		n = size(P)(1); % num rows in P
 		forces = zeros(n,n,2);
@@ -31,12 +35,14 @@ function tester(iterations)
 		F = zeros(n,2);
 
 		for i=1:n
-			for j=i+1:n
-				F(i,1) = F(i,1) + forces(i,j,1);
-				F(i,2) = F(i,2) + forces(i,j,2);
+			for j=1:n
+				% F(i,1) = F(i,1) + forces(i,j,1);
+				% F(i,2) = F(i,2) + forces(i,j,2);
 
-				F(j,1) = F(j,1) - forces(i,j,1);
-				F(j,2) = F(j,2) - forces(i,j,2);
+				% F(j,1) = F(j,1) - forces(i,j,1);
+				% F(j,2) = F(j,2) - forces(i,j,2);
+				F(i,1) += forces(i,j,1);
+				F(i,2) += forces(i,j,2);
 			end
 		end
 
@@ -47,17 +53,21 @@ function tester(iterations)
 			V(i,:) = V(i,:) + H*F(i,:)*(1/M(i));
 		end
 
-		P
+		% P
 		% V
 	end
+
+	P
 end
 
 function force = calcforce(m1, m2, p1x, p1y, p2x, p2y)
+	pixelsPerMeter = 10000;
+
 	s1 = [p1x;p1y];
 	s2 = [p2x;p2y];
 
 	G = 6.673*10^-11;
-	d = norm(s1-s2);
+	d = norm(s1-s2) / pixelsPerMeter;
 	force = -G * ((m1*m2)/d^3) * (s1 - s2);
 end
 
